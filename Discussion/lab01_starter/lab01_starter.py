8fdc9a5e2dfe92dffccedea3896cc2c032c22242
Yi Liu <154867456+yliu2702@users.noreply.github.com>
2024-09-17 13:16:10 -0400

    Update lab01_starter.ipynb

--- Discussion/lab01_starter/lab01_starter.py
+++ Discussion/lab01_starter/lab01_starter.py
@@ -39,3 +39,54 @@ soup.find('table') #find_all()
 # find string
 soup.find(string = "List Item One").parent.parent
 
+soup.find('a')
+
+# Get the Country and Population Table and store it in SQLite3 database
+
+wiki_url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+
+response = requests.get(wiki_url)
+print(response.status_code == 200)
+
+soup = BeautifulSoup(response.content, 'html')
+
+tables = soup.find_all('table')
+len(tables)
+
+# find the class of desired table
+for table in tables:
+  if table.find(string = '1 Jul 2024'):
+    print(table['class'])
+
+table = soup.find('table', class_ = ['wikitable', 'sortable', 'sticky-header', 'sort-under', 'mw-datatable', 'col2left', 'col6left'])
+
+table_1 = soup.find_all('table',{'class': 'wikitable'})
+
+type(table),type(table_1) # want Tag for further operation
+
+for row in table.find('tbody').find_all('tr')[2:]:
+  print("The Location is: ", row.find_all('td')[1].text)
+  print("The population is ",row.find_all('td')[2].text)
+  break
+
+countries = []
+populations = []
+for i, row in enumerate(table.find('tbody').find_all('tr')[2:]):
+  if i == 1:
+    countries.append(row.find_all('td')[1].text.strip())
+    populations.append(row.find_all('td')[2].text.strip())
+    print("The Location is: ", row.find_all('td')[1].text)
+    print("The population is ",row.find_all('td')[2].text)
+  else:
+    countries.append(row.find_all('td')[1].text.strip())
+    populations.append(row.find_all('td')[2].text.strip())
+
+    print("The Location is: ",row.find_all('td')[1].text)
+    print("The population is ",row.find_all('td')[2].text)
+
+import pandas as pd
+import sqlite3
+df = pd.DataFrame.from_dict({'countryname': countries, 'population':populations})
+conn = sqlite3.connect('somedatabase.db')
+df.to_sql('populations', conn, if_exists='replace')
+
